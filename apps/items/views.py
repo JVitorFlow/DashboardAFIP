278168f39766bbb22cacc4554eda5a38384177c5
João Vitor <joaosilvadev10@gmail.com>
2025-02-05 00:27:10 -0300

    Corrige referências ao Celery Beat no tasks.py

--- apps/items/views.py
+++ apps/items/views.py
@@ -14,12 +14,16 @@ from collections import defaultdict
 from apps.items.models import Item
 from apps.tasks.models import Task
 from .serializer import TaskSummarySerializer, ItemSerializer
-from apps.api.utils import check_id, check_if_can_change_status
+
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.exceptions import NotFound
-from apps.values.models import ShiftData
+from rest_framework.permissions import AllowAny
 
+from django.views.generic.edit import UpdateView
+from django.urls import reverse_lazy
+from .forms import ShiftDataForm
+from datetime import datetime
 class ItemViewSet(viewsets.ModelViewSet):
     """
     ViewSet para visualizar, editar e gerenciar itens.
@@ -172,6 +176,90 @@ class ItemViewSet(viewsets.ModelViewSet):
             return Response(serializer.data, status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+    @swagger_auto_schema(
+        operation_description="Autoriza a continuação do processamento do item.",
+        operation_summary="Autorizar Item",
+        responses={200: ItemSerializer}
+    )
+    @action(detail=True, methods=['patch'], url_path='authorize', permission_classes=[AllowAny])
+    def authorize(self, request, pk=None):
+        """
+        Atualiza o campo is_authorized para True para autorizar a continuação do item.
+        
+        Retorna:
+        - Response: Confirmação da autorização.
+        """
+        try:
+            item = Item.objects.get(id=pk)
+        except Item.DoesNotExist:
+            raise NotFound(detail="Item não encontrado.")
+
+        # Atualiza o campo is_authorized para True
+        item.is_authorized = True
+        item.save()
+
+        serializer = ItemSerializer(item)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=["get"], url_path="sismama-data")
+    def get_sismama_data(self, request):
+        """
+        Endpoint para obter dados do SISMAMA (apenas itens autorizados com dados do Shift).
+        """
+        # Filtrar itens autorizados
+        authorized_items = (
+            Item.objects.filter(is_authorized=True, stage="SISMAMA")
+            .select_related("task_id")  # Evita múltiplas consultas para o Task
+            .prefetch_related("shift_data")  # Otimiza acesso ao ShiftData
+        )
+
+        # Buscar dados relacionados do ShiftData
+        sismama_data = []
+        for item in authorized_items:
+            shift_data = item.shift_data.first()  # Assumindo que `shift_data` é uma relação OneToMany
+            if shift_data:
+                sismama_data.append({
+                    "item_id": item.id,
+                    "os_number": item.os_number,
+                    "os_name": item.os_name,
+                    "created_at": item.created_at,
+                    "started_at": item.started_at,
+                    "ended_at": item.ended_at,
+                    "status": item.status,
+                    "stage": item.stage,
+                    "shift_result": item.shift_result,
+                    "image_result": item.image_result, 
+                    "is_authorized": item.is_authorized,
+                    # Dados do ShiftData
+                    "shift_data": {
+                        "cnes": shift_data.cnes,
+                        "cartao_sus": shift_data.cartao_sus,
+                        "nome_paciente": shift_data.nome_paciente,
+                        "sexo": shift_data.sexo,
+                        "raca_etinia": shift_data.raca_etinia,
+                        "idade_paciente": shift_data.idade_paciente,
+                        "data_nascimento": shift_data.data_nascimento,
+                        "data_coleta": shift_data.data_coleta,
+                        "data_liberacao": shift_data.data_liberacao,
+                        "tamanho_lesao": shift_data.tamanho_lesao,
+                        "caracteristica_lesao": shift_data.caracteristica_lesao,
+                        "localizacao_lesao": shift_data.localizacao_lesao,
+                        "logradouro": shift_data.logradouro,
+                        "codigo_postal": shift_data.codigo_postal,
+                        "numero_residencial": shift_data.numero_residencial,
+                        "cidade": shift_data.cidade,
+                        "estado": shift_data.estado,
+                    }
+                })
+
+        # Retornar a resposta
+        if not sismama_data:
+            return Response({"detail": "Nenhum dado autorizado encontrado para SISMAMA."}, status=status.HTTP_404_NOT_FOUND)
+
+        return Response(sismama_data, status=status.HTTP_200_OK)
+
 
 class ItemListView(LoginRequiredMixin, ListView):
     """
@@ -205,3 +293,97 @@ class ItemListView(LoginRequiredMixin, ListView):
         context['task'] = self.task
         context['segment'] = 'items'
         return context
+
+
+class ItemUpdateView(LoginRequiredMixin, UpdateView):
+    model = Item
+    fields = [] 
+    template_name = 'includes/modal_default.html'
+
+    def get_success_url(self):
+        # Redireciona para a lista de itens na tarefa
+        item = self.get_object()
+        return reverse('items', args=[item.task_id])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Obter dados de ShiftData associados ao Item
+        item = self.get_object()
+        shift_data = item.shift_data.first()
+
+        # Passar ShiftDataForm ao contexto para renderização
+        context['shift_data_form'] = ShiftDataForm(instance=shift_data)
+        return context
+
+    def form_valid(self, form):
+        """Atualiza os dados do Item e dos campos relacionados."""
+
+        # Atualizar dados do Item (como `image_result`)
+        response = super().form_valid(form)
+
+        # Atualizar campos de `image_result`, se enviados
+        self.update_item_image_result()
+
+        # Atualizar campos de ShiftData, se enviados
+        self.update_shift_data()
+
+        return response
+
+    def update_item_image_result(self):
+        """Atualizar apenas o campo image_result do Item."""
+        image_result = self.request.POST.get('image_result')
+        if image_result is not None:
+            item = self.get_object()
+            item.image_result = image_result
+            item.save()
+
+    def update_shift_data(self):
+        """Atualizar apenas os campos de ShiftData."""
+        # Obter o objeto ShiftData associado ao Item
+        shift_data = self.get_object().shift_data.first()
+
+        if not shift_data:
+            # Se não existir ShiftData, não faz nada
+            return
+
+        # Atualizar apenas os campos enviados no formulário
+        shift_data_fields = self.get_shift_data_fields()
+        for field, value in shift_data_fields.items():
+            if value is not None:  # Apenas atualize se o campo foi enviado
+                setattr(shift_data, field, value)
+
+        # Salvar mudanças no banco de dados
+        shift_data.save()
+
+    def get_shift_data_fields(self):
+        """Extrair campos enviados pelo formulário relacionados ao ShiftData."""
+        return {
+            'cnes': self.request.POST.get('cnes'),
+            'os_number': self.request.POST.get('os_number'),
+            'cartao_sus': self.request.POST.get('cartao_sus'),
+            'nome_paciente': self.request.POST.get('nome_paciente'),
+            'sexo': self.request.POST.get('sexo'),
+            'raca_etinia': self.request.POST.get('raca_etinia'),
+            'idade_paciente': self.request.POST.get('idade_paciente'),
+            'data_nascimento': self.parse_date(self.request.POST.get('data_nascimento')),
+            'data_coleta': self.parse_date(self.request.POST.get('data_coleta')),
+            'data_liberacao': self.parse_date(self.request.POST.get('data_liberacao')),
+            'tamanho_lesao': self.request.POST.get('tamanho_lesao'),
+            'caracteristica_lesao': self.request.POST.get('caracteristica_lesao'),
+            'localizacao_lesao': self.request.POST.get('localizacao_lesao'),
+            'codigo_postal': self.request.POST.get('codigo_postal'),
+            'logradouro': self.request.POST.get('logradouro'),
+            'numero_residencial': self.request.POST.get('numero_residencial'),
+            'cidade': self.request.POST.get('cidade'),
+            'estado': self.request.POST.get('estado'),
+        }
+
+    @staticmethod
+    def parse_date(date_str):
+        """Converte data do formato 'YYYY-MM-DD' ou retorna None."""
+        try:
+            return datetime.strptime(date_str, '%Y-%m-%d').date()
+        except (ValueError, TypeError):
+            return None
+
+
--- apps/robots/tasks.py
+++ apps/robots/tasks.py
@@ -1,30 +1,69 @@
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
 from django_celery_beat.models import PeriodicTask, IntervalSchedule
-from apps.tasks.utils import get_tasks_from_items
-from apps.items.utils import get_items
-from apps.robots.utils import (check_disconnected_robots, change_status_inactive,
-                          assing_robots, get_min_robot, get_active_robots,
-                          remove_robots)
 from apps.core.celery import app
+from apps.robots.utils import (
+    check_disconnected_robots,
+    change_status_inactive,
+    get_active_robots,
+    get_min_robot,
+    assing_robots,
+    remove_robots,
+)
+from apps.items.utils import get_items
+from apps.tasks.utils import get_tasks_from_items
 from apps.core.memory_manager import get_memory
 
-# Remover intervalos duplicados se existirem
-IntervalSchedule.objects.filter(every=3, period=IntervalSchedule.MINUTES).exclude(pk=IntervalSchedule.objects.filter(every=3, period=IntervalSchedule.MINUTES).first().pk).delete()
 
-# Obter ou criar um cronograma de intervalo para executar a tarefa a cada 3 minutos
-schedule_handle, _ = IntervalSchedule.objects.get_or_create(
-    every=3,
-    period=IntervalSchedule.MINUTES,
-)
+# Função para configurar intervalos e tarefas após as migrações
+@receiver(post_migrate)
+def setup_periodic_tasks(sender, **kwargs):
+    # Remover intervalos duplicados de 3 minutos
+    IntervalSchedule.objects.filter(every=3, period=IntervalSchedule.MINUTES).exclude(
+        pk=IntervalSchedule.objects.filter(every=3, period=IntervalSchedule.MINUTES)
+        .first()
+        .pk
+    ).delete()
 
-# Tente obter uma tarefa periódica existente ou crie uma nova
-periodic_task_handle, _ = PeriodicTask.objects.update_or_create(
-    name='handle_disconnected_robots',
-    defaults={
-        'interval': schedule_handle,
-        'task': 'robots.tasks.handle_disconnected_robots',
-        'enabled': True,
-    },
-)
+    # Obter ou criar um cronograma de intervalo de 3 minutos
+    schedule_handle, _ = IntervalSchedule.objects.get_or_create(
+        every=3,
+        period=IntervalSchedule.MINUTES,
+    )
+
+    # Criar ou atualizar a tarefa periódica "handle_disconnected_robots"
+    PeriodicTask.objects.update_or_create(
+        name="handle_disconnected_robots",
+        defaults={
+            "interval": schedule_handle,
+            "task": "robots.tasks.handle_disconnected_robots",
+            "enabled": True,
+        },
+    )
+
+    # Remover intervalos duplicados de 1 minuto
+    IntervalSchedule.objects.filter(every=1, period=IntervalSchedule.MINUTES).exclude(
+        pk=IntervalSchedule.objects.filter(every=1, period=IntervalSchedule.MINUTES)
+        .first()
+        .pk
+    ).delete()
+
+    # Obter ou criar um cronograma de intervalo de 1 minuto
+    schedule_check, _ = IntervalSchedule.objects.get_or_create(
+        every=1,
+        period=IntervalSchedule.MINUTES,
+    )
+
+    # Criar ou atualizar a tarefa periódica "check_robots_every_minute"
+    PeriodicTask.objects.update_or_create(
+        name="check_robots_every_minute",
+        defaults={
+            "interval": schedule_check,
+            "task": "robots.tasks.check_robots_every_minute",
+            "enabled": False,  # Tarefa começa desativada
+        },
+    )
+
 
 @app.task
 def handle_disconnected_robots():
@@ -38,35 +77,19 @@ def handle_disconnected_robots():
             remove_robots([items, tasks])
             items = items.none()
             tasks = tasks.none()
+
     robots_active = get_active_robots()
     if not robots_active:
         periodic_task_check.enabled = True
         periodic_task_check.save()
 
-# Remover intervalos duplicados se existirem
-IntervalSchedule.objects.filter(every=1, period=IntervalSchedule.MINUTES).exclude(pk=IntervalSchedule.objects.filter(every=1, period=IntervalSchedule.MINUTES).first().pk).delete()
-
-# Obter ou criar o intervalo de agendamento para a tarefa periódica
-schedule_check, _ = IntervalSchedule.objects.get_or_create(
-    every=1,
-    period=IntervalSchedule.MINUTES,
-)
-
-# Tente obter ou criar a tarefa periódica para verificar robôs
-periodic_task_check, _ = PeriodicTask.objects.update_or_create(
-    name='check_robots_every_minute',
-    defaults={
-        'interval': schedule_check,
-        'task': 'robots.tasks.check_robots_every_minute',
-        'enabled': False,  # A tarefa começa desativada
-    },
-)
 
 @app.task
 def check_robots_every_minute():
     if periodic_task_handle.enabled:
         periodic_task_handle.enabled = False
         periodic_task_handle.save()
+
     memory = get_memory()
     robots = get_active_robots()
     if robots:
